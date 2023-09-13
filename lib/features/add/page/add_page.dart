0486dadcc3d9256230eb1c5b439282d8ec2c1e11
Kacper Majcher <kacpermajcher13@gmail.com>
2023-09-13 20:36:37 +0200

    Pop add screen on save

--- lib/features/add/page/add_page.dart
+++ lib/features/add/page/add_page.dart
@@ -26,73 +26,79 @@ class _AddPageState extends State<AddPage> {
     return BlocProvider(
       /*BlocProvider w ktorym inicjujemy AddCubit*/
       create: (context) => AddCubit(),
-      child: BlocBuilder<AddCubit, AddState>(
-        /*BlocBuilder który wywołuje się za każdym razem gdy state AddCubit'a się zmieni */
-        builder: (context, state) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Add new upcoming title'),
-              /*Nowy parametr dla AppBar'a - actions. Jest to lista widgetów dla actions
-              w którem możemy sobie dostarczać np IconButton,co jest ikonką z prawej
-              lub lewej strony AppBara (najczesciej check lub X)*/
-              actions: [
-                IconButton(
-                  /*Na onPressed w tym IconButton to weryfikacja; jesli choć jedna zmienna
-                  jest równa null - na onPressed podajemy null, a addbutton jest nieaktywny,
-                  inaczej dostarczamy całą funkcję i dodajemy dane do cubita*/
-                  onPressed: _imageURL == null ||
-                          _title == null ||
-                          _releaseDate == null
-                      ? null
-                      : () {
-                          context.read<AddCubit>().add(
-                                /*wykrzyknik oznacza, ze string na pewno nie przechowuje nulla*/
-                                _title!,
-                                _imageURL!,
-                                _releaseDate!,
-                              );
-                        },
-                  icon: const Icon(
-                      Icons.check), //ta ikonka wywołuje metodę add na cubicie
-                ),
-              ],
-            ),
-            /*Tutaj posiadamy body przechowujacy prywatna klase _AddPageBody, któremu 
-            musimy dostarczyc az 4 elementy.
-            onTitleChanged
-            onImageUrlChanged
-            onDateChanged
-            selectedDateFormatted
-            */
-            body: _AddPageBody(
-              onTitleChanged: (newValue) {
-                //co jesli zmieni sie onTitle
-                setState(() {
-                  _title = newValue; //przypisz nową wartosc i wywolaj setState
-                });
-              },
-              onImageUrlChanged: (newValue) {
-                //co jesli zmieni sie onIamge
-                setState(() {
-                  _imageURL =
-                      newValue; //przypisz nową wartosc i wywolaj setState
-                });
-              },
-              onDateChanged: (newValue) {
-                //co jesl zmieni sie onDate
-                setState(() {
-                  _releaseDate =
-                      newValue; //przypisz nową wartosc i wywolaj setState
-                });
-              },
-              selectedDateFormatted: _releaseDate?.toIso8601String(),
-              /*jaka jest aktualnie wybrana data - domyslnie null. 
-              Pytajnik mowi nam, ze jesli to null, nie probuj nawet tego konwertowac
-              jesli cos jest zamiast nulla, przekonwetuj to na stringa, 
-              przypisz nową wartosc i wywolaj setState*/
-            ),
-          );
+      child: BlocListener<AddCubit, AddState>(
+        listener: (context, state) {
+          Navigator.of(context).pop();
         },
+        child: BlocBuilder<AddCubit, AddState>(
+          /*BlocBuilder który wywołuje się za każdym razem gdy state AddCubit'a się zmieni */
+          builder: (context, state) {
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('Add new upcoming title'),
+                /*Nowy parametr dla AppBar'a - actions. Jest to lista widgetów dla actions
+                    w którem możemy sobie dostarczać np IconButton,co jest ikonką z prawej
+                    lub lewej strony AppBara (najczesciej check lub X)*/
+                actions: [
+                  IconButton(
+                    /*Na onPressed w tym IconButton to weryfikacja; jesli choć jedna zmienna
+                        jest równa null - na onPressed podajemy null, a addbutton jest nieaktywny,
+                        inaczej dostarczamy całą funkcję i dodajemy dane do cubita*/
+                    onPressed: _imageURL == null ||
+                            _title == null ||
+                            _releaseDate == null
+                        ? null
+                        : () {
+                            context.read<AddCubit>().add(
+                                  /*wykrzyknik oznacza, ze string na pewno nie przechowuje nulla*/
+                                  _title!,
+                                  _imageURL!,
+                                  _releaseDate!,
+                                );
+                          },
+                    icon: const Icon(
+                        Icons.check), //ta ikonka wywołuje metodę add na cubicie
+                  ),
+                ],
+              ),
+              /*Tutaj posiadamy body przechowujacy prywatna klase _AddPageBody, któremu 
+                  musimy dostarczyc az 4 elementy.
+                  onTitleChanged
+                  onImageUrlChanged
+                  onDateChanged
+                  selectedDateFormatted
+                  */
+              body: _AddPageBody(
+                onTitleChanged: (newValue) {
+                  //co jesli zmieni sie onTitle
+                  setState(() {
+                    _title =
+                        newValue; //przypisz nową wartosc i wywolaj setState
+                  });
+                },
+                onImageUrlChanged: (newValue) {
+                  //co jesli zmieni sie onIamge
+                  setState(() {
+                    _imageURL =
+                        newValue; //przypisz nową wartosc i wywolaj setState
+                  });
+                },
+                onDateChanged: (newValue) {
+                  //co jesl zmieni sie onDate
+                  setState(() {
+                    _releaseDate =
+                        newValue; //przypisz nową wartosc i wywolaj setState
+                  });
+                },
+                selectedDateFormatted: _releaseDate?.toIso8601String(),
+                /*jaka jest aktualnie wybrana data - domyslnie null. 
+                    Pytajnik mowi nam, ze jesli to null, nie probuj nawet tego konwertowac
+                    jesli cos jest zamiast nulla, przekonwetuj to na stringa, 
+                    przypisz nową wartosc i wywolaj setState*/
+              ),
+            );
+          },
+        ),
       ),
     );
   }
